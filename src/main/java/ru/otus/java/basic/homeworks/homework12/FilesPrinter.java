1cac5da1fdc0cd1fa8ffe82d848e3ce5c058e583
Nikolay Shatskikh <nikolay.shatskikh@edna.ru>
2024-06-07 10:34:33 +0300

    minor fixes

--- src/main/java/ru/otus/java/basic/homeworks/homework12/FilesPrinter.java
+++ src/main/java/ru/otus/java/basic/homeworks/homework12/FilesPrinter.java
@@ -1,30 +1,33 @@
 package ru.otus.java.basic.homeworks.homework12;
 
 import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 
 public class FilesPrinter {
-    private static final String rootPath = "./src/main/resources/files-hm12";
-    private static String path = rootPath;
+    private static final String ROOT_PATH = "./src/main/resources/files-hm12";
+    private static String path = ROOT_PATH;
 
-    private static final String separator = "-----------------------------------------";
+    private static final String SEPARATOR = "-----------------------------------------";
 
     public static void printFiles() {
         File root = new File(path);
 
         File[] listFiles = root.listFiles();
         if (listFiles != null) {
-            System.out.println(separator);
+            System.out.println(SEPARATOR);
             for (File file : listFiles) {
                 System.out.println(file.getName());
             }
-            System.out.println(separator);
+            System.out.println(SEPARATOR);
         }
 
         String selectedFile = FileOperations.selectFile();
         if (selectedFile.equals("exit")) {
             return;
         }
-        String newPath = path + "/" + selectedFile;
+        Path concatedPath = Paths.get(path,selectedFile);
+        String newPath = concatedPath.toString();
         File newFile = new File(newPath);
         if (newFile.exists()) {
             if (newFile.isDirectory()) {
